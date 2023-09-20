d0443ed5c3fd1d3a0c348e60966ef98a8db04f9d
dubeysunny0593 <145573762+dubeysunny0593@users.noreply.github.com>
2023-09-20 20:18:24 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,21 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
 
 contract Tokening {
-    string public tokenName = "Code";
-    string public tokenSymbol = "Super Code";
-    uint256 public totalSupply = 0;
+
+    string public tokenName="bitcoin";
+    string public tokenSymbol="BTC";
+    uint256 public totalSupply=0;
+
     mapping(address => uint256) public balances;
 
-    function mint(address _to, uint256 _value) external {
-        require(_to != address(0), "Invalid address");
+    function mint(address _to, uint256 _value) public {
         totalSupply += _value;
         balances[_to] += _value;
     }
 
-    function burn(uint256 _value) external {
-        require(balances[msg.sender] >= _value, "Insufficient balance");
+    function burn(uint256 _value) public {
+        require(balances[msg.sender] >= _value, "Sorry! You Don't Have Enough Money");
         totalSupply -= _value;
         balances[msg.sender] -= _value;
     }
+
 }
